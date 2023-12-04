336cc8492e9a2fc7fac63e0687284aac4bb6f62d
akbarpn136 <akbarpn136@gmail.com>
2023-12-04 21:05:46 +0700

    Penyederhanaan modul main

--- dafreq/main.py
+++ dafreq/main.py
@@ -1,2 +1,13 @@
+from layout.default import DefaultLayout
+
+
 def run():
-  print("Init DaFreq")
+    app = DefaultLayout(
+        title="DaFreq LA3",
+        themename="darkly",
+        iconphoto="logo.png",
+        minsize=(800, 600),
+    )
+
+    app.place_window_center()
+    app.mainloop()
